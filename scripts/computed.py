b0ada72f1595163ec9c946e8847f7965cc5c829b
joelgomes1994 <joelgomes1994@hotmail.com>
2022-01-14 13:51:38 -0300

    feat: add bgforce v0.0.4 to project

---
+++ scripts/computed.py
@@ -0,0 +1,8 @@
+""" Custom computed properties. Reference: https://bgempire.github.io/bgforce/variables/computed """
+
+import bge
+from .bgf import config, database, state, _
+
+
+# def computedProperty():
+#     return "A computed property: " + bge.app.version_string
---
+++ scripts/operators.py
@@ -0,0 +1,9 @@
+"""Custom operator definitions. Reference: https://bgempire.github.io/bgforce/operators/custom """
+
+import bge
+
+
+# Custom operators
+# def customOperator(cont, args=""):
+#     pass
+
