c005f9513e867b2564913a48332956ca08462127
joelgomes1994 <joelgomes1994@hotmail.com>
2022-01-23 18:30:02 -0300

    fix(player): fix z velocity

--- scripts/player.py
+++ scripts/player.py
@@ -140,6 +140,7 @@ def __move(cont):
     
     runFactor = MOVE_RUN_MULTIPLIER if own["Run"] else 1.0
     moveVector = Vector([-own["MoveH"], -own["MoveV"], 0]).normalized() * MOVE_SPEED_FACTOR * runFactor
+    moveVector.z = own.localLinearVelocity.z
     own.localLinearVelocity = moveVector
 
 
