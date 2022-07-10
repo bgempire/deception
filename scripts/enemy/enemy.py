0ed1cbde0c5758160e9897aa4615488e9d7d8ba0
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-07-10 16:57:08 -0300

    refactor(enemy): rewrite to subclass format

--- scripts/enemy/enemy.py
+++ scripts/enemy/enemy.py
@@ -4,18 +4,28 @@ from bge.types import *
 
 def enemy(cont):
     # type: (SCA_PythonController) -> None
-    
+
+    own = cont.owner # type: Enemy
     always = cont.sensors["Always"] # type: SCA_AlwaysSensor
-    
+
     if always.positive:
-        
+
         if always.status == bge.logic.KX_SENSOR_JUST_ACTIVATED:
-            __init(cont)
-            
-        # TODO
+            own = Enemy(own, cont)
+
+        own.update()
 
 
-def __init(cont):
-    # type: (SCA_PythonController) -> None
-    
-    pass
+class Enemy(KX_GameObject):
+
+    def __init__(self, obj, cont):
+        # type: (KX_GameObject, SCA_PythonController) -> None
+
+        self.currentController = cont # type: SCA_PythonController
+        """The current controller of the enemy."""
+
+
+    def update(self):
+        # type: () -> None
+
+        pass
